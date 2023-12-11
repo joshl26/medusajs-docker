29b4468ed7701022a169e48a5141d84b206583a9
Joshua Lehman <joshlehman.dev@gmail.com>
2023-12-10 23:28:16 -0500

    updates

--- backend/develop.sh
+++ backend/develop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+#Start development environment
+medusa develop
+
 #Run migrations to ensure the database is updated
 
-medusa seed -f seed.json
+medusa seed -f ./seed.json
 
 medusa migrations run
 
 medusa user -e some@email.com -p some-password
 
-#Start development environment
-medusa develop
--- storefront/.next/static/chunks/webpack.js
+++ storefront/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa180e882785e5a0"; }
+/******/ 		__webpack_require__.h = function() { return "145fec5cc8fb8ea8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
